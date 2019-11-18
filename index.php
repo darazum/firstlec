ba59458956289fe05ddaab3b65292aa580086ed0
darazum <razumovskiy@playform.com>
2019-11-18 21:11:23 +0300

    func

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-echo 1234;
-echo 1234;
-echo 1234;
-echo 1234;
-echo 1234;
-echo 1234;
-echo 1234;
-echo 1234;
-echo 'sdljnfwlekfnfieifhsdkfhafuashdlfah';
-echo 'sdljfhsdkfhafuashdlfah';
-echo 'sdljfhsdkfhafnfwlekfnfieiuashdlfah';
-echo 'sdljfhsdkfhafuashdlfah';
-echo 'sdljfhsdkfhafuashdlfah';
-echo 'sdljfhsdkfnfwlekfnfieihafuashdlfah';
+function a()
+{
+    echo 123;
+}
